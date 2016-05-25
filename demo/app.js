2a3452f118e7fab59a820daa96d4601400617263
Jamison Prianos <jprianos@accusoft.com>
2016-05-25 15:16:45 -0400

    Serve files from a static directory on the file system

--- demo/app.js
+++ demo/app.js
@@ -1,12 +1,23 @@
 var http = require('http');
+var fs = require('fs');
 
 function contentServer(req, res) {
-  if (req.url === '/index.html') {
-    res.end('Hello, I am index.html');
-  } else {
-    res.statusCode = 404;
-    res.end('404 Error');
-  }
+  var filename = __dirname + '/public' + req.url;
+  fs.exists(filename, exists => {
+    if (exists) {
+      fs.readFile(filename, (err, content) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end('500 Error');
+        } else {
+          res.end(content);
+        }
+      });
+    } else {
+      res.statusCode = 404;
+      res.end('404 Error');
+    }
+  });
 }
 
 http.createServer(contentServer).listen(8080);
