460fb524e0f4622be46aa6b5baad54c4baaea341
Jamison Prianos <jprianos@accusoft.com>
2016-05-25 15:19:22 -0400

    Move to Express as a web app server

--- demo/app.js
+++ demo/app.js
@@ -1,23 +1,6 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var server = express();
 
-function contentServer(req, res) {
-  var filename = __dirname + '/public' + req.url;
-  fs.exists(filename, exists => {
-    if (exists) {
-      fs.readFile(filename, (err, content) => {
-        if (err) {
-          res.statusCode = 500;
-          res.end('500 Error');
-        } else {
-          res.end(content);
-        }
-      });
-    } else {
-      res.statusCode = 404;
-      res.end('404 Error');
-    }
-  });
-}
+server.use(express.static('public'));
 
-http.createServer(contentServer).listen(8080);
+server.listen(8080);
