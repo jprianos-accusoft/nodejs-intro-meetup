e83612ebe12f65cac79580cadf2545d3b6b8e9e8
Jamison Prianos <jprianos@accusoft.com>
2016-05-25 15:15:05 -0400

    Handle routing for a specific endpoint

--- demo/app.js
+++ demo/app.js
@@ -1,5 +1,12 @@
 var http = require('http');
 
-http.createServer((req, res) => {
-  res.end('Hello, World!');
-}).listen(8080);
+function contentServer(req, res) {
+  if (req.url === '/index.html') {
+    res.end('Hello, I am index.html');
+  } else {
+    res.statusCode = 404;
+    res.end('404 Error');
+  }
+}
+
+http.createServer(contentServer).listen(8080);
