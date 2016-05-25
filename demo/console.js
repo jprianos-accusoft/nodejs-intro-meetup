21f5b65a2e5df03b6aca38e6e484ce31c08fc828
Jamison Prianos <jprianos@accusoft.com>
2016-05-25 16:32:03 -0400

    Support access via command line in console.js

---
+++ demo/console.js
@@ -0,0 +1,12 @@
+var operations = require('./operations.js');
+
+var op = process.argv[2];
+var values = process.argv[3];
+
+if (operations[op]) {
+  var numbers = values.split(',');
+  var result = operations[op](numbers);
+  console.log(result);
+} else {
+  console.log('The following operations are supported: ' + Object.keys(operations).join(', '));
+}
